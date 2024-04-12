fb695b7bf28975d15e441c527c58a85521b3dc4c
AleksandrasSalenga123 <aleks.salenga@gmail.com>
2024-04-12 01:07:07 +0100

    Projectile fixed

--- Assets/Nova/WizardProjectileScript.cs
+++ Assets/Nova/WizardProjectileScript.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WizardProjectileScript : MonoBehaviour
 {
     public float speed = 5f;
     public Rigidbody2D rb;
-    public PlayerHealthScript PlayerHealthScript;
+    private GameObject Player;
+    private PlayerHealthScript PlayerHealthScript;
 
-
+    private void Awake()
+    {
+        Player = GameObject.FindWithTag("Player");
+        PlayerHealthScript = Player.GetComponent<PlayerHealthScript>();
+    }
     void Update()
     {
 
@@ -21,7 +27,8 @@ public class WizardProjectileScript : MonoBehaviour
         if (other.tag == "Player")
         {
             Debug.Log("Hit");
-            PlayerHealthScript.Health = -0.25f;
+            PlayerHealthScript.Health -= 0.25f;
+            Destroy(gameObject);
         }
     }
 }
