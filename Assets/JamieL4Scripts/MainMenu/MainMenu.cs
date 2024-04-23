1d4fd0d807f412cad5fb53705f6881d94248d438
AleksandrasSalenga123 <aleks.salenga@gmail.com>
2024-04-23 23:56:03 +0100

    Fixed a buggy script

--- Assets/JamieL4Scripts/MainMenu/MainMenu.cs
+++ Assets/JamieL4Scripts/MainMenu/MainMenu.cs
@@ -1,23 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManager;
+using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour {
+public class MainMenu : MonoBehaviour 
+{
 
-    public void PlayGame()
+  //  public void PlayGame();
    
-    void Start()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
-    }
+ //   void Start()
+ //   {
+ //       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+ //   }
 
   
    
-    {
-        public void QuitGame ()
-        Debug.Log("Quit!");
-        Application.Quit();
+ //   {
+  //      public void QuitGame();
+  //      Debug.Log("Quit!");
+    //    Application.Quit();
    
-    }
+  //  }
 }
+
