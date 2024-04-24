7229617823d47a06c1e3d6d4d2d20f645a80faf2
AgentJamie <jamie.newman04@hotmail.com>
2024-04-24 18:53:32 +0100

    mainmenu
    bug fix

--- Assets/Scenes/MainMenu/MainMenu.cs
+++ Assets/Scenes/MainMenu/MainMenu.cs
@@ -1,23 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManager;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class MainMenu : MonoBehaviour 
+{
 
-public class MainMenu : MonoBehaviour {
-
-    public void PlayGame()
+    
    
     void Start()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+        
     }
-
+        public void Quit()
   
    
     {
-        public void QuitGame ()
+        
         Debug.Log("Quit!");
-        Application.Quit();
+       Application.Quit();
    
-    }
+   }
 }
+
