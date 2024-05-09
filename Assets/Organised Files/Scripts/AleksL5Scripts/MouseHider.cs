a374223bef76f41d9eddfb49c6046dc997152e34
Feedpompano <aleks.salenga@gmail.com>
2024-05-09 11:51:34 +0100

    Added crosshair and debugged bullets not going through transition triggers

--- Assets/Organised Files/Scripts/AleksL5Scripts/MouseHider.cs
+++ Assets/Organised Files/Scripts/AleksL5Scripts/MouseHider.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class MouseHider : MonoBehaviour
 {
-    // Start is called before the first frame update
+
     void Start()
     {  
         Cursor.visible = false;
+ 
     }
     // Update is called once per frame
     void Update()
