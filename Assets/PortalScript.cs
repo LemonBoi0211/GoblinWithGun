aa3dce15b8abf158f908f9dd45a9309106f8f7ae
Feedpompano <aleks.salenga@gmail.com>
2024-05-16 11:49:57 +0100

    Revert "Added colours to different dungeons"
    This reverts commit 8be8785703513fe107122008fa4f0826a9b80bad.

--- Assets/PortalScript.cs
+++ Assets/PortalScript.cs
@@ -1,21 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class PortalScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    private void OnTriggerEnter2D(Collider2D other)
+    void Start()
     {
-        if (other.tag == "Bullet")
-        {
-            SceneManager.LoadScene(0);
-        }
+        
     }
 
-        // Update is called once per frame
-        void Update()
+    // Update is called once per frame
+    void Update()
     {
         
     }
